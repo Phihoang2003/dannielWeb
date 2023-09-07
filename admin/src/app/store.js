206792bf64b745a4e568382392293f7aa4b796cf
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-09-07 22:12:23 +0700

    comple productSlice and Productlist

--- admin/src/app/store.js
+++ admin/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../feature/auth/authSlice.js";
 import customerReducer from "../feature/customer/customerSlice.js";
+import productReducer from "../feature/product/productLSlice.js"
 export const store=configureStore({
     reducer:{
         auth:authReducer,
         customer: customerReducer,
+        product: productReducer,
     }
 })
