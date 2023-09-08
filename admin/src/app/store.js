c55c540e01242490d480b67eb255c2d819a7925b
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-09-08 18:26:57 +0700

    update brandModel and brandCtrl

--- admin/src/app/store.js
+++ admin/src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../feature/auth/authSlice.js";
 import customerReducer from "../feature/customer/customerSlice.js";
 import productReducer from "../feature/product/productLSlice.js"
+import brandReducer from "../feature/brand/brandSlice.js"
 export const store=configureStore({
     reducer:{
         auth:authReducer,
         customer: customerReducer,
         product: productReducer,
+        brand: brandReducer,
     }
 })
