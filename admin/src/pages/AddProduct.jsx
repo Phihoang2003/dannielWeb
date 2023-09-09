79af0f3c635f481d5d1e75be34ab119105f0a931
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-09-09 15:07:03 +0700

    continue AddProduct

--- admin/src/pages/AddProduct.jsx
+++ admin/src/pages/AddProduct.jsx
@@ -1,39 +1,248 @@
-import { useState } from "react";
+import { React, useEffect, useState } from "react";
 import CustomInput from "../component/CustomInput";
 import ReactQuill from "react-quill";
+import { useNavigate } from "react-router-dom";
 import "react-quill/dist/quill.snow.css";
-const AddProduct=()=>{
-    const [desc,setDesc]=useState();
-    const handleDesc=(e)=>{
-        console.log(e);
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { getBrands } from "../feature/brand/brandSlice";
+import { getCategories } from "../feature/pcategory/pcategorySlice";
+import { getColors } from "../feature/color/colorSlice";
+import { Select } from "antd";
+import {useDropzone} from "react-dropzone";
+
+import { createProducts, resetState } from "../feature/product/productLSlice";
+let schema = yup.object().shape({
+  title: yup.string().required("Title is Required"),
+  description: yup.string().required("Description is Required"),
+  price: yup.number().required("Price is Required"),
+  brand: yup.string().required("Brand is Required"),
+  category: yup.string().required("Category is Required"),
+  tags: yup.string().required("Tag is Required"),
+  color: yup
+    .array()
+    .min(1, "Pick at least one color")
+    .required("Color is Required"),
+  quantity: yup.number().required("Quantity is Required"),
+});
+
+const AddProduct = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [color, setColor] = useState([]);
+  console.log(color);
+  useEffect(() => {
+    dispatch(getBrands());
+    dispatch(getCategories());
+    dispatch(getColors());
+  }, []);
+
+  const brandState = useSelector((state) => state.brand.brands);
+  const catState = useSelector((state) => state.pCategory.pCategories);
+  const colorState = useSelector((state) => state.color.colors);
+  const newProduct = useSelector((state) => state.product);
+  const { isSuccess, isError, isLoading, createdProduct } = newProduct;
+  useEffect(() => {
+    if (isSuccess && createdProduct) {
+      toast.success("Product Added Successfullly!");
     }
-    return(
-        <div>
-            <h3 className="mb-4">Add Product</h3>
-            <div>
-                <form action="">
-                    <CustomInput type='text' label='Enter Product Title'/>
-                    <div className="mb-3">
-                        <ReactQuill theme="snow" value={desc} onChange={(etv)=>handleDesc(etv)}/>
-                    </div>
-                    <CustomInput type='number' label='Enter Product Price'/>
-                    <select name="" id="" className="form-control py-3  mt-3">
-                        <option value="">Select Brand</option>
-                    </select>
-                    <select name="" id="" className="form-control py-3  mt-3">
-                        <option value="">Select Category</option>
-                    </select>
-                    <select name="" id="" className="form-control py-3  mt-3">
-                        <option value="">Select Color</option>
-                    </select>
-                    <CustomInput type='number' label='Enter Product Price'/>
-                    <button className="btn btn-success border-0 rounded-3 my-5">
-                            Add Product
-                    </button>
-                </form>
-            </div>
+    if (isError) {
+      toast.error("Something Went Wrong!");
+    }
+  }, [isSuccess, isError, isLoading]);
+  const coloropt = [];
+  colorState.forEach((i) => {
+    coloropt.push({
+      label: i.title,
+      value: i._id,
+    });
+  });
+
+  useEffect(() => {
+    formik.values.color = color ? color : " ";
+  }, [color]);
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+      price: "",
+      brand: "",
+      category: "",
+      tags: "",
+      color: "",
+      quantity: "",
+      images:""
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(createProducts(values));
+      formik.resetForm();
+      setColor(null);
+      setTimeout(() => {
+        dispatch(resetState());
+      }, 3000);
+    },
+  });
+  const handleColors = (e) => {
+    setColor(e);
+    console.log(color);
+  };
+  function DropzoneWithoutClick(props) {
+    const {getRootProps, acceptedFiles} = useDropzone();
+    const files = acceptedFiles.map(file => <li key={file.path}>{file.path}</li>);
+  
+    return (
+      <section className="container">
+        <div {...getRootProps({className: 'dropzone'})}>
+          <p>Dropzone without click events</p>
         </div>
-    )
-}
+        <aside>
+          <h4>Files</h4>
+          <ul>{files}</ul>
+        </aside>
+      </section>
+    );
+  }
+  return (
+    <div>
+      <h3 className="mb-4 title">Add Product</h3>
+      <div>
+        <form
+          onSubmit={formik.handleSubmit}
+          className="d-flex gap-3 flex-column"
+        >
+          <CustomInput
+            type="text"
+            label="Enter Product Title"
+            name="title"
+            onCh={formik.handleChange("title")}
+            onBl={formik.handleBlur("title")}
+            val={formik.values.title}
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
+          <div className="">
+            <ReactQuill
+              theme="snow"
+              name="description"
+              onChange={formik.handleChange("description")}
+              value={formik.values.description}
+            />
+          </div>
+          <div className="error">
+            {formik.touched.description && formik.errors.description}
+          </div>
+          <CustomInput
+            type="number"
+            label="Enter Product Price"
+            name="price"
+            onCh={formik.handleChange("price")}
+            onBl={formik.handleBlur("price")}
+            val={formik.values.price}
+          />
+          <div className="error">
+            {formik.touched.price && formik.errors.price}
+          </div>
+          <select
+            name="brand"
+            onChange={formik.handleChange("brand")}
+            onBlur={formik.handleBlur("brand")}
+            value={formik.values.brand}
+            className="form-control py-3 mb-3"
+            id=""
+          >
+            <option value="">Select Brand</option>
+            {brandState.map((i, j) => {
+              return (
+                <option key={j} value={i.title}>
+                  {i.title}
+                </option>
+              );
+            })}
+          </select>
+          <div className="error">
+            {formik.touched.brand && formik.errors.brand}
+          </div>
+          <select
+            name="category"
+            onChange={formik.handleChange("category")}
+            onBlur={formik.handleBlur("category")}
+            value={formik.values.category}
+            className="form-control py-3 mb-3"
+            id=""
+          >
+            <option value="">Select Category</option>
+            {catState.map((i, j) => {
+              return (
+                <option key={j} value={i.title}>
+                  {i.title}
+                </option>
+              );
+            })}
+          </select>
+          <div className="error">
+            {formik.touched.category && formik.errors.category}
+          </div>
+          <select
+            name="tags"
+            onChange={formik.handleChange("tags")}
+            onBlur={formik.handleBlur("tags")}
+            value={formik.values.tags}
+            className="form-control py-3 mb-3"
+            id=""
+          >
+            <option value="" disabled>
+              Select Category
+            </option>
+            <option value="featured">Featured</option>
+            <option value="popular">Popular</option>
+            <option value="special">Special</option>
+          </select>
+          <div className="error">
+            {formik.touched.tags && formik.errors.tags}
+          </div>
+
+          <Select
+            mode="multiple"
+            allowClear
+            className="w-100"
+            placeholder="Select colors"
+            defaultValue={color}
+            onChange={(i) => handleColors(i)}
+            options={coloropt}
+          />
+          <div className="error">
+            {formik.touched.color && formik.errors.color}
+          </div>
+          <CustomInput
+            type="number"
+            label="Enter Product Quantity"
+            name="quantity"
+            onCh={formik.handleChange("quantity")}
+            onBl={formik.handleBlur("quantity")}
+            val={formik.values.quantity}
+          />
+          <div className="error">
+            {formik.touched.quantity && formik.errors.quantity}
+          </div>
+          <div className="bg-white border-1 p-5 text-center">
+            
+          <DropzoneWithoutClick />
+          </div>
+
+          <button
+            className="btn btn-success border-0 rounded-3 my-5"
+            type="submit"
+          >
+            Add Product
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default AddProduct;
