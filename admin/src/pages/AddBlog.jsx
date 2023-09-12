9621fc7795531e5f182e7f8d05fb3148bfec151b
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-09-12 13:57:50 +0700

    update blog config

--- admin/src/pages/AddBlog.jsx
+++ admin/src/pages/AddBlog.jsx
@@ -4,26 +4,12 @@ import CustomInput from "../component/CustomInput";
 
 import { toast } from "react-toastify";
 import { useState } from "react";
-
+import {uploadImg,delImg} from "../feature/upload/uploadSlice"
 const AddBlog=()=>{
-    const [desc,setDesc]=useState();
-    const handleDesc=(e)=>{
-        console.log(e);
-    }
+    
     return(
         <div>
-            <h3 className="mb-4">Add Blog</h3>
-            <div>
-                <form action="">
-                    <CustomInput type='text' label='Enter Blog Title'/>
-                    <select name="" id="" className="form-control py-3  mt-3">
-                        <option value="">Select Blog Category</option>
-                    </select>
-                    <ReactQuill theme="snow" value={desc} onChange={(etv)=>handleDesc(etv)}/>
-
-                    <button className="btn btn-success border-0 rounded-3 my-5">Add Blog </button>
-                </form>
-            </div>
+           
         </div>
     )
 }
