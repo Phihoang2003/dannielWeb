30312dcbb44785321626f151fd753cb8d33c487d
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-09-17 13:36:20 +0700

    create route get update and delete

--- admin/src/feature/product/productService.js
+++ admin/src/feature/product/productService.js
@@ -13,9 +13,35 @@ const createProduct = async (product) => {
   return response.data; 
 };
 
+const deleteProduct=async(productId)=>{
+  const response=await axios.delete(`${base_url}product/${productId}`)
+  return response.data
+}
+const updateProduct=async(product)=>{
+  const response=await axios.put(`${base_url}product/${product.id}`,{
+    title:product.productData.title,
+    description:product.productData.description,
+    category:product.productData.category,
+    brand:product.productData.brand,
+    price:product.productData.price,
+    color:product.productData.color,
+    quantity:product.productData.quantity,
+    images:product.productData.images,
+    tags:product.productData.tags
+
+  });
+  return response.data;
+}
+const getAProduct=async(productId)=>{
+  const response=await axios.get(`${base_url}product/${productId}`);
+  return response.data
+}
 const productService = {
   getProducts,
   createProduct,
+  deleteProduct,
+  updateProduct,
+  getAProduct
 };
 
 export default productService;
