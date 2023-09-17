527c9989f4ddcad519b0bac786ff82edcf3dee68
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-09-17 22:09:35 +0700

    add auth to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit"
 import productReducer from "../features/products/productSlice"
+import authReducer from "../features/user/userSlice"
 export const store=configureStore({
     reducer:{
+        auth:authReducer,
         product:productReducer
     }
 })
