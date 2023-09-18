52798d479b3da548d263ace18aaa2246947f11ab
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-09-18 19:51:03 +0700

    add logic and register user in client

--- client/src/features/user/userService.js
+++ client/src/features/user/userService.js
@@ -1,12 +1,22 @@
-import axios from "axios"
-import {base_url} from "../../utils/base_url" 
-const register=async(userData)=>{
-    const res=await axios.post(`${base_url}user/register`,userData);
-    if(res.data){
-        return res.data
-    }
-}
+import axios from "axios";
+import { base_url } from "../../utils/base_url";
+const register = async (userData) => {
+  const res = await axios.post(`${base_url}user/register`, userData);
+  if (res.data) {
+    return res.data;
+  }
+};
 
-export const authService={
-    register
-}
+const login = async (useData) => {
+    console.log({useData});
+  const res = await axios.post(`${base_url}user/login`, useData);
+  if (res.data) {
+    localStorage.setItem("customer", JSON.stringify(res.data));
+    }
+    return res.data;
+};
+
+export const authService = {
+  register,
+  login,
+};
